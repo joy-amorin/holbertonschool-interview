7e9ea0696e167b5c92db98b2399eb53618653a59
Johana Amorin <amorinkaren2021@gmail.com>
2024-01-03 08:09:01 -0800

    lockboxes/

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+
+def canUnlockAll(boxes):
+    """ Method that determinates
+    if all th boxescan be opened """
+
+    if not boxes or not boxes[0]:
+        return False
+
+    open_box = set([0])
+    pending_box = set([0])
+
+    while pending_box:
+        current_box = pending_box.pop()
+
+        for key in boxes[current_box]:
+            if key not in open_box:
+                open_box.add(key)
+                pending_box.add(key)
+    return len(open_box) == len(boxes)
